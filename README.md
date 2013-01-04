692754237a193636c6bbe0370aed04e3a467a0ad
Philip Lawrence <philip@misterphilip.com>
2013-01-04 00:56:22 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -2,3 +2,27 @@ ipb-syntaxhighlighter
 =====================
 
 SyntaxHighlighter Drop-in replacement for IPB 3.4.x
+
+## Installation
+
+Installation of the [SyntaxHighlighter](http://alexgorbatchev.com/SyntaxHighlighter/) code is fairly straightforward: 
+ 
+ 1. Upload the files from the uploads directory, similar to how you would for an IPB install / upgrade
+ 1. Within the `/installables` you'll find a few different XML files. 
+    1. `bbcode_SyntaxHighlighter.xml` is a BB Code replacement for the code bbcode. 
+    You should delete that bbcode and install this one. Alternatively, you can change the 
+    following items in the default code bbcode:
+        * Custom BBCode Tag: codesyntax
+        * Custom BBCode Aliases: code,codebox
+        * OR PHP file to execute: codesyntax.php
+    1. `hook_SyntaxHighlighter.xml` is a (skin overloading) hook that replaces the prettify code with the 
+    SyntaxHighlighter code.  You should install this like a normal hook (Applications & Modules  >  Manage Hooks 
+    > Install Hook)
+ 1. At this point, all new posts will be using SyntaxHighlighter. If you have existing posts with code, 
+    you'll likely want to rebuild the content of them (Tools & Settings  >  Recount & Rebuild > Rebuild Content: Post Content)
+
+## Styling
+
+With SyntaxHighlighter comes [themes](http://alexgorbatchev.com/SyntaxHighlighter/manual/themes/). 
+By default, the core & default theme are loaded. You can change this by replacing `/public/style_css/3rd_party/SyntaxHighlighter/shTheme.css` 
+with the theme of your choice.
---
+++ upload/admin/sources/classes/text/parser/bbcode/codesyntax.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * SyntaxHighlighter drop-in replacement for IP.Board v3.4.1
+ *
+ * @author 		Philip Lawrence
+ * @copyright	(c) 2012 - 2013 Philip Lawrence
+ * @license		http://www.invisionpower.com/company/standards.php#license
+ * @package		IP.Board
+ * @link		http://misterphilip.com
+ * @version     10000
+ *
+ */
+
+// Load in the current plugin class
+if( !class_exists('bbcode_plugin_code') )
+{
+	require_once( IPS_ROOT_PATH . 'sources/classes/text/parser/bbcode/defaults.php' );/*noLibHook*/
+}
+
+class bbcode_plugin_codesyntax extends bbcode_plugin_code
+{   
+    
+	/**
+	 * {@inherit}
+	 */
+	public function __construct( ipsRegistry $registry, $_parent='' )
+	{
+		$this->currentBbcode	= 'codesyntax';
+        $this->_parent = $_parent;
+		
+		parent::__construct( $registry, $_parent );
+	}
+	
+	/**
+	 * Do the actual replacement
+	 *
+	 * @access	protected
+	 * @param	string		$txt	Parsed text from database to be edited
+	 * @return	string				BBCode content, ready for editing
+	 */
+	protected function _replaceText( $txt )
+	{
+        // Check for the correct class, less resource intensive than regex
+        if( stristr( $txt, '_prettyXPrint' ) )
+        {
+            // We already have the HTML for it, let's clean it up and GeSHi it
+            if( preg_match_all( '#<pre[^>]*class\s?=\s?(["\'])((?:(?!\1).)*)_prettyXprint((?:(?!\1).)*)\1[^>]*>((?:(?!</pre>).)*)</pre>#is', $txt, $matches ) > 0 )
+            {
+                $codeboxCount = count( $matches[0] );
+                
+                // Loop through all of our matches
+                for($i = 0; $i < $codeboxCount; $i++)
+                {
+                    $options = array();
+                    
+                    // Grab the other classes (options in this case) 
+                    list($options['lang'], $options['lineNum'] ) = explode(' ', trim( $matches[2][$i] . $matches[3][$i] ) );
+                    $options['lang'] = str_replace( '_lang-', '', $options['lang'] );
+                    $options['lineNum'] = str_replace( '_linenums:', '', $options['lineNum'] );
+                    
+                    // Replace the current with the new
+                    $replacement = $this->_colorfy( $matches[4][$i], $options );
+                    $txt = str_replace( $matches[0][$i] , $replacement, $txt );
+                }
+            }
+        }
+        
+        // Run the existing for bbcode versions
+		parent::_replaceText( $txt );
+        
+        // Return the awesome looking code
+		return $txt;
+	}
+    
+	/**
+	 * {@inherit}
+	 */
+	protected function _buildOutput( $content, $option )
+	{
+        // This is the original, used for BBCode.. which could cause issues with HTML (e.g. <br> in source)
+        
+		$content        = trim( $content );
+		
+		$content = preg_replace( '#(<br(?:[^>]+?)?>)#i', '', $content );
+		$content = trim( $content );
+		
+		$content = str_replace( '<!-preserve.newline-->', "\n", $content );
+        
+        // Grab the option
+        $lineNums = 1;
+		$langAdd  = '';
+		
+		if ( !is_array( $option ) )
+		{
+            list( $option['lang'], $option['lineNum']) = explode( ':', $option );
+		}
+		
+        // Make it pretty!
+        return $this->__colorfy( $content, $option );
+	}
+    
+
+	/**
+	 * GeSHi the code
+	 *
+	 * @access	protected
+	 * @param	string      $content   Content to prettify
+	 * @param	array       $options   [Optional] code options
+	 * @return	string			       Content ready for SyntaxHighlighter
+	 */
+	protected function _colorfy( $content, array $options )
+	{
+        // Convert the languages from the current editor
+        switch( $options['lang'] )
+        {
+            case 'js': 
+                $options['lang'] = 'javascript';
+            break;
+            case 'html':
+                $options['lang'] = 'html5';
+            break;
+            case 'auto':
+                $this->_defaultLanguage;
+            break;
+            default:
+                // Nothing to do here, we'll check in just a second on these
+            break;
+        }
+        
+        // Verify we have the language in the array
+        if( ! in_array( $options['lang'], $this->_allowedLanguages ) )
+        {
+            $options['lang'] = $this->_defaultLanguage;
+        }
+        
+        $finalContent = '<pre class="';
+        $finalContent.= 'brush: ' . $options['lang'] . ';';
+        
+        // Settings -----------------------------------------------
+        // -> Line numbers
+        $lineNums = intval( $options['lineNum'] );
+        if( $lineNums > 0 )
+        {
+            $finalContent.= ' first-line: ' . $lineNums . ';';
+        }
+        
+        $finalContent.= '> ';
+        $finalContent.= $content;
+        $finalContent.= '</pre>';
+        
+        // Return our parsed code
+		return $finalContent;
+	}
+
+}
